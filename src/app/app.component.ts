55c0e6ef818b26a40e0f9c2ec1a4458912f7808a
TomaszSokalski <tsokalski88@gmail.com>
2022-10-12 20:50:08 +0200

    Add sidenav and logic to open and close

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+ 
   title = 'angularNewsApp';
+  @ViewChild(MatSidenav) sideNav!: MatSidenav; // Catch MatSidenav to variable and looking for changes on DOM 
+  
+  constructor(private observer : BreakpointObserver){}
+
+
+  ngAfterViewInit(): void {
+    this.sideNav.opened = true;
+    this.observer.observe(['(max-width:800px)'])
+    .subscribe((res)=>{
+      if(res?.matches){
+        this.sideNav.mode="over";
+        this.sideNav.close();
+      }else{
+        this.sideNav.mode = 'side';
+        this.sideNav.open();
+      }
+    })
+  }
 }
