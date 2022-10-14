17c8a4afb517a908924b6be1d177a8c45a43d72d
TomaszSokalski <tsokalski88@gmail.com>
2022-10-14 08:31:32 +0200

    Add api_key

--- src/app/service/news.service.ts
+++ src/app/service/news.service.ts
@@ -6,7 +6,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class NewsService {
 
-  api_key = 'your api key';
+  api_key = '95c9e5cfc4644ee2aff121ff459a4706';
 
   constructor(private http: HttpClient) { }
 
